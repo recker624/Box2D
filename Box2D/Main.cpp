268fc98dc23c8a5686285b444aa79f5d5ce74fad
recker624 <52703428+recker624@users.noreply.github.com>
2021-01-24 00:49:08 +0530

    The box can move now!

--- Box2D/Main.cpp
+++ Box2D/Main.cpp
@@ -2,13 +2,19 @@
 #include<GLFW/glfw3.h>
 
 #include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
 
 #define WIN_HEIGHT 600
 #define WIN_WIDTH 800
 
 void framebuffer_resize_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-void createBufferObjects(unsigned int& VBO, unsigned int& VAO, unsigned int& EBO);
+void processInput(GLFWwindow *window, unsigned int shaderProgram, unsigned int uniformVarLocation, GLfloat& moveDisX, GLfloat& moveDisY);
+void createGLObjects(unsigned int& VBO, unsigned int& VAO, unsigned int& EBO);
+void createShaderProgram(unsigned int& shaderProgram);
+std::string getShaderSource(std::string fileName);
+void checkShaderCompileAndLinkErrors(unsigned int ID, std::string type);
 
 int main() {
 	//initialise glfw
@@ -45,15 +51,28 @@ int main() {
 
 	//createBufferObjects(vbo, vao, ebo);
 	unsigned int vbo, vao, ebo;
-	createBufferObjects(vbo, vao, ebo);
+	createGLObjects(vbo, vao, ebo);
+
+	unsigned int shaderProgram;
+	createShaderProgram(shaderProgram);
+
+	unsigned int uniformVarLocation = glGetUniformLocation(shaderProgram, "updatePos");
+	glUniform3f(uniformVarLocation, 0, 0, 0);
+	float moveDisX = 0;
+	float moveDisY = 0;
 
 	//render loop
 	while (!glfwWindowShouldClose(window)) {
 
-		processInput(window);
+		processInput(window, shaderProgram, uniformVarLocation, moveDisX, moveDisY);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -63,26 +82,130 @@ int main() {
 	return 0;
 }
 
-void createBufferObjects(unsigned int& vbo, unsigned int& vao, unsigned int& ebo) {
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
-	//generate a buffer object
-	glGenBuffers(1, &vbo);
-	//bind the buffer object to a buffer type
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	//copy the vertex data into the buffer object
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices, GL_STATIC_DRAW);
 
-	
+
+
+
+
+void createGLObjects(unsigned int& vbo, unsigned int& vao, unsigned int& ebo) {
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	unsigned int indices[] = { 
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+	//bind the object to a type
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	//copy the vertex data into the buffer object
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//tell opengl how to interpret the vertex data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+}
+
+void createShaderProgram(unsigned int& shaderProgram) {
+	unsigned int vertexShader, fragmentShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//attach shader with the source code
+	std::string vertexStrCode = getShaderSource("vertex.vs");
+	std::string fragmentStrCode = getShaderSource("fragment.txt");
+	const char* vShaderSourceCode = vertexStrCode.c_str();
+	const char* fShaderSourceCode = fragmentStrCode.c_str();
+
+	glShaderSource(vertexShader, 1, &vShaderSourceCode, NULL);
+	glCompileShader(vertexShader);
+	checkShaderCompileAndLinkErrors(vertexShader, "VERTEX");
+
+	glShaderSource(fragmentShader, 1, &fShaderSourceCode, NULL);
+	glCompileShader(fragmentShader);
+	checkShaderCompileAndLinkErrors(fragmentShader, "FRAGMENT");
+
+	//create the shader program by attaching the shaders 
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	checkShaderCompileAndLinkErrors(shaderProgram, "PROGRAM");
 
 }
 
-void processInput(GLFWwindow* window) {
+//reads the shader text file and returns a string containing the source code
+std::string getShaderSource(std::string fileName) {
+	std::string sourceCode;
+	std::ifstream shaderFile(fileName);
+	if (shaderFile.is_open()) {
+		//ensure ifstream objects can throw exceptions
+		shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try {
+			//create stringstreams and read the file data into raw buffer
+			std::stringstream shaderStream;
+			shaderStream << shaderFile.rdbuf();
+			//close the files
+			shaderFile.close();
+			//read the buffer contents into a string
+			sourceCode = shaderStream.str();
+		}
+		catch (std::ifstream::failure& e) {
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+		}
+	}
+	return sourceCode;
+}
+
+void checkShaderCompileAndLinkErrors(unsigned int ID, std::string type) {
+	int success;
+	char infolog[1024];
+	//check what kind of object it is
+	if (type == "VERTEX" || type == "FRAGMENT") {
+		glGetShaderiv(ID, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(ID, 1024, NULL, infolog);
+			std::cout << "ERROR::SHADER::" << type << "::COMPILATION_FAILED" << infolog << std::endl;
+		}
+	}
+	else if (type == "PROGRAM") {
+		glGetProgramiv(ID, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(ID, 1024, NULL, infolog);
+			std::cout << "ERROR::SHADER::PROGRAM" << type << "::LINKING_FAILED" << infolog << std::endl;
+		}
+	}
+	
+}
+
+void processInput(GLFWwindow* window, unsigned int shaderProgram, unsigned int uniformVarLocation, GLfloat& moveDisX, GLfloat& moveDisY) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		moveDisY += 0.01;
+		glUniform3f(uniformVarLocation, moveDisX, moveDisY, 0);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		moveDisY -= 0.01;
+		glUniform3f(uniformVarLocation, moveDisX, moveDisY, 0);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		moveDisX += 0.01;
+		glUniform3f(uniformVarLocation, moveDisX, moveDisY, 0);
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		moveDisX -= 0.01;
+		glUniform3f(uniformVarLocation, moveDisX, moveDisY, 0);
 	}
 }
 
--- Box2D/vertex.vs
+++ Box2D/vertex.vs
@@ -1,7 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+uniform vec3 updatePos;
+
+
 void main(){
 	//an inbuilt variable in glsl to take in the position data of the vertices
-	gl_Position = vec4(aPos, 1.0);
+	gl_Position = vec4((aPos/10) + updatePos, 1.0);
 }
